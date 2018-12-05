b216c4086ca5620fff761a758bf4b6b697b03bb2
AlessandraMarchesi <45562023+AlessandraMarchesi@users.noreply.github.com>
2018-12-05 22:48:54 +0100

    Update functions.php
    Added a limit to the max running races

--- functions.php
+++ functions.php
@@ -3,17 +3,26 @@
 
   if (isset($_POST['start'])) {
 
-        
+        // CHECK how many races are running (limit: max running races)
+      $act_races = mysqli_query ($conn, "SELECT COUNT(race_end) AS num FROM race WHERE race_end < 1");  
+         // race end indicates race open and closed 
+            
+      $row_races = mysqli_fetch_array($act_races);
+          // if there are 3 races running, the page returns an error
+          if ($row_races['num'] >= $running) {
+                
+          } else {
+     
         // create the race
-        mysqli_query ($conn, "INSERT INTO race (distance, race_end) VALUES ($racemt,0");
+        mysqli_query ($conn, "INSERT INTO race (distance, race_end) VALUES ($racemt,0)");
             
         $i = 1;
         // counting the number of horses needed
         while ($i <= $horsenum) {
         
             // generating the horses' stats
-            $stats = array(rand($statmin,$statmax)/10, rand($statmin,$statmax)/10, rand($statmin,$statmax)/10);
-            
+            $stats = array(mt_rand($statmin,$statmax)/10, mt_rand($statmin,$statmax)/10, mt_rand($statmin,$statmax)/10);
+
             // inserting the horses' stats in the db
             mysqli_query ($conn, "INSERT INTO horse (endurance,speed,strength,horse_time,horse_dist) VALUES ($stats[0],$stats[1],$stats[2],0,$racemt)"); 
             
@@ -21,7 +30,8 @@
             mysqli_query ($conn, "INSERT INTO clghorserace (clg_race,clg_horse) VALUES ((SELECT race_id FROM race ORDER BY race_id DESC LIMIT 1),(SELECT horse_id FROM horse ORDER BY horse_id DESC LIMIT 1))");
             
             $i = $i+1;
-       }
+       } //while
+     } // else
  }
             
 //*** FUNCTION FOR 1 SEC OF ADVANCEMENT OF THE RACE ***//
@@ -73,30 +83,44 @@
                 
                 // GIVE THE HORSES A POSITION
                         // get the active races
-                        $act_races = mysqli_query ($conn, "SELECT *  FROM race ORDER BY race_id DESC LIMIT 1");  //change limit 1 with the max active races //
+                        $act_races = mysqli_query ($conn, "SELECT *  FROM race WHERE race_end = 0 ORDER BY race_id DESC LIMIT $running");  // race end indicates race open and closed
             
                         // get each race
                          while ($row_act = mysqli_fetch_array($act_races)) {
                              $distances = mysqli_query ($conn, "SELECT horse_id, horse_dist, horse_pos, finish, race_id, clghorserace.* FROM race JOIN clghorserace ON race_id = clg_race JOIN horse ON horse_id = clg_horse WHERE race_id = $row_act[race_id] ORDER BY horse_dist ASC"); 
                              
                              // positions start from 1
-                            $pos = 1;
-                             
+                            $pos = 1;                             
                              // loop to give everyone - from top distance to bot - a position
                             while (($pos <= $horsenum) && ($row_dist = mysqli_fetch_array($distances))) {
                                 
                                 // if the horse has arrived, the position doesn't change and the value just increase for the next horse to come
                                 if ($row_dist['finish'] > 0){
-                                $pos = $pos +1;
+                                $pos = $pos+1;
                                 } else {
                                 // otherwise it uploads
                                 mysqli_query ($conn, "UPDATE horse SET horse_pos = $pos WHERE horse_id = $row_dist[horse_id]");
-                                $pos = $pos +1;
-                                }
-                                
-                            } 
-                         }
-        
+                                $pos = $pos+1;
+                                }//if
+                            } //while                             
+                         }//while 
+            
+                mysqli_free_result($act_races);
+            
+               // CHECK THE RACES FOR ENDED ONES (limit: max running races)
+               $act_races = mysqli_query ($conn, "SELECT *  FROM race WHERE race_end < 1 ORDER BY race_id DESC LIMIT $running");  // race end indicates race open and closed 
+                       
+                     while ($row_actives = mysqli_fetch_array($act_races)) {
+                         // get horses which have finished
+                         $horses = mysqli_query ($conn, "SELECT COUNT(finish) AS num FROM horse JOIN clghorserace ON horse_id = clg_horse JOIN race ON race_id = clg_race WHERE race_id = $row_actives[race_id] AND finish > 0"); 
+                            $row_horses = mysqli_fetch_array($horses);
+                         
+                         // if they are all the horses in the race, I "close" the race
+                         if ($row_horses['num'] < $horsenum) {
+                         } else {
+                             mysqli_query ($conn, "UPDATE race SET race_end = 1 WHERE race_id = $row_actives[race_id]");
+                         } 
+                     }
         };
 
 /**** CALL THE FUNCTION FOR 1 SEC OF ADVANCEMENT ****/
